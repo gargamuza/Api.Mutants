86da92d3937bbafc29522d5507591596ffda208d
pablo <pablozoff@hotmail.com>
2021-07-14 19:59:52 -0300

    Validacion para la longitud minima de los elementos que componen una coleccion

--- Api.Mutants/CustomRequestValidations/StringsInCollectionHaveMinimumLenght.cs
+++ Api.Mutants/CustomRequestValidations/StringsInCollectionHaveMinimumLenght.cs
@@ -1,5 +1,4 @@
-﻿using Api.Mutants.Configuration;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -8,22 +7,27 @@ using System.Threading.Tasks;
 
 namespace Api.Mutants.CustomRequestValidations
 {
-    public class StringsInCollectionHaveSameLenght : ValidationAttribute
-    {    
+    public class StringsInCollectionHaveMinimumLenght : ValidationAttribute
+    {
+        private readonly int MINIMUN_LENGHT;
+        public StringsInCollectionHaveMinimumLenght(int minimumLenght, string errorMessage) : base(errorMessage)
+        {
+            MINIMUN_LENGHT = minimumLenght;
+        }
+
         public override bool IsValid(object value)
         {
             var list = value as IList;
             if (list == null || list.Count < 2)
                 return false;
-            
+
             for (int i = 0; i < list.Count - 1; i++)
             {
-                if (list[i]?.ToString().Length != list[i + 1]?.ToString().Length)
+                if (list[i]?.ToString().Length < MINIMUN_LENGHT)
                     return false;
-            }               
-            
+            }
+
             return true;
         }
     }
 }
-
--- Api.Mutants/Models/Requests/MutantRequest.cs
+++ Api.Mutants/Models/Requests/MutantRequest.cs
@@ -8,6 +8,7 @@ namespace Api.Mutants.Models.Request
         [Required]
         [StringsInCollectionOnlyHaveValidLetters("ATCG", "Only the following letters are allowed: ATCG")]
         [StringsInCollectionHaveSameLenght(ErrorMessage = "The words that make up DNA must be the same length.")]
+        [StringsInCollectionHaveMinimumLenght(4, "The words that make up the DNA must be at least 4 characters long")]
         public string[] dna { get; set; }
     }
 }
