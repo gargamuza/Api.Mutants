8eb24500c5bad6f02f054f1a288b692d5ac48a60
pablo <pablozoff@hotmail.com>
2021-07-14 20:08:51 -0300

    Se agrega validacion para que el numero de elementos del dna coincida con la longitud de las palabras para poder armar el Array de NxN

---
+++ Api.Mutants/CustomRequestValidations/StringsInCollectionHaveSameLenghtAndQuantity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Mutants.CustomRequestValidations
+{
+    public class StringsInCollectionHaveSameLenghtAndQuantity : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var list = value as IList;
+            if (list == null || list.Count < 2)
+                return false;
+
+            var count = list.Count;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i]?.ToString().Length != count)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
--- Api.Mutants/Models/Requests/MutantRequest.cs
+++ Api.Mutants/Models/Requests/MutantRequest.cs
@@ -9,6 +9,7 @@ namespace Api.Mutants.Models.Request
         [StringsInCollectionOnlyHaveValidLetters("ATCG", "Only the following letters are allowed: ATCG")]
         [StringsInCollectionHaveSameLenght(ErrorMessage = "The words that make up DNA must be the same length.")]
         [StringsInCollectionHaveMinimumLenght(4, "The words that make up the DNA must be at least 4 characters long")]
+        [StringsInCollectionHaveSameLenghtAndQuantity(ErrorMessage = "The words that make up the DNA must have the same length as the number of words")]
         public string[] dna { get; set; }
     }
 }
