0aebed972b3cb401c0403d4048810095be66384d
pablo <pablozoff@hotmail.com>
2021-07-17 02:19:09 -0300

    Se agrega proyecto pruebas unitarias

---
+++ Api.Mutants.Testing/StringsInCollectionHaveMinimumLenghtTest.cs
@@ -0,0 +1,26 @@
+﻿using Api.Mutants.CustomRequestValidations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Mutants.Testing
+{
+    [TestClass]
+    class StringsInCollectionHaveMinimumLenghtTest
+    {
+        [TestMethod]
+        public void TestIsValid_AtLeastOneElementIsLessThatMinimun()
+        {
+            //Arrange
+            var stringsInCollectionHaveMinimumLenght = new StringsInCollectionHaveMinimumLenght(4, "");
+            var array = new string[] { "ABCD", "ABC", "EFGB" };
+
+            //Act
+            var result = stringsInCollectionHaveMinimumLenght.IsValid(array);
+
+            //Fact
+            Assert.IsFalse(result);
+        }
+    }
+}
